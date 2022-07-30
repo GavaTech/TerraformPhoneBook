100a8c51aba04f6a218a8c8f4d0275238e578135
GavaTech <james_adamsp@yahoo.com>
2022-07-30 16:18:47 -0500

    update

---
+++ tf-files/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.24.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "4.28.0"
+    }    
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  
+//  access_key = ""
+//  secret_key = ""
+# Configuration options
+}
+
+provider "github" {
+  # Configuration options
+  token = "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"
+}
